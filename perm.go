75b0a140a43376d3e1b8a5d4e3892f3c143f4c64
MISUMI Masaru <deltam@users.noreply.github.com>
2019-07-12 01:18:59 +0900

    Change algorythm sigma,delta -> sigma,tau

--- perm.go
+++ perm.go
@@ -45,8 +45,8 @@ func (p *Perm) Next() {
 		return
 	}
 	if check(p.cur) {
-		delta(p.cur)
-		deltaSlice(p.slice)
+		tau(p.cur)
+		tauSlice(p.slice)
 	} else {
 		sigma(p.cur)
 		sigmaSlice(p.slice)
@@ -60,21 +60,27 @@ func (p Perm) Done() bool {
 
 func startPerm(n int) []int {
 	start := make([]int, n)
-	for i := 0; i < n-2; i++ {
-		start[i] = n - i - 2
+	start[0] = n - 2
+	start[1] = n - 1
+	for i := 2; i < n; i++ {
+		start[i] = n - i - 1
 	}
-	start[n-2] = n - 1
-	start[n-1] = 0
 	return start
 }
 
 func (p Perm) isLast() bool {
 	n := len(p.cur)
-	if p.cur[0] != 0 || p.cur[1] != 1 {
+	if p.cur[n-1] != n-1 {
 		return false
 	}
-	for i := 2; i < n; i++ {
-		if p.cur[i] != n-i+1 {
+	for i := 0; i < n-1; i++ {
+		j := i
+		if i == 0 {
+			j = 1
+		} else if i == 1 {
+			j = 0
+		}
+		if p.cur[i] != n-j-2 {
 			return false
 		}
 	}
@@ -89,14 +95,8 @@ func sigma(p []int) {
 	p[n-1] = f
 }
 
-// 2 4 5 ... n-1 1 0
-func delta(p []int) {
-	n := len(p)
-	f0 := p[0]
-	f1 := p[1]
-	copy(p[0:n-2], p[2:n])
-	p[n-2] = f1
-	p[n-1] = f0
+func tau(p []int) {
+	p[1], p[0] = p[0], p[1]
 }
 
 func sigmaSlice(slice interface{}) {
@@ -111,42 +111,38 @@ func sigmaSlice(slice interface{}) {
 	}
 }
 
-func deltaSlice(slice interface{}) {
+func tauSlice(slice interface{}) {
 	if slice == nil {
 		return
 	}
-	rv := reflect.ValueOf(slice)
 	swap := reflect.Swapper(slice)
-	n := rv.Len()
-	for i := 2; i < n; i++ {
-		swap(i-2, i)
-	}
-	if n%2 == 0 {
-		swap(n-2, n-1)
-	}
+	swap(0, 1)
 }
 
 func check(p []int) bool {
 	n := len(p)
-	if p[0] == n-1 {
+	if p[1] == n-1 {
 		return false
 	}
 	pos := 0
 	for i := 0; i < n; i++ {
+		if i == 1 {
+			continue
+		}
 		if p[i] == n-1 {
 			pos = i
 			break
 		}
 	}
-	pos++
-	if pos == n {
-		pos = 1
+	pos = (pos + 1) % n
+	if pos == 1 {
+		pos = 2
 	}
-	if p[pos] != (p[0]-2+n)%(n-1) {
+	if p[pos%n] != (p[1]-2+n)%(n-1) {
 		return false
 	}
 	for i := 0; i < n; i++ {
-		if p[(i+1)%n] != n-i-1 {
+		if p[i] != n-i-1 {
 			return true
 		}
 	}
