a7a8f55f1fb1a9a307bb31734596a13bf1213e70
MISUMI Masaru <deltam@users.noreply.github.com>
2021-11-13 14:46:06 +0900

    Refactored Benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,25 +2,29 @@ package perm
 
 import "testing"
 
-const benchNum = 10
+const benchNum = 7
 
 func BenchmarkPrimitive(b *testing.B) {
 	p := make([]int, benchNum)
-	if err := Init(p); err != nil {
-		b.Fatalf("Init() failed: %v", err)
-	}
-	for !IsEnd(p) {
-		Advance(p)
+	for i := 0; i < b.N; i++ {
+		if err := Init(p); err != nil {
+			b.Fatalf("Init() failed: %v", err)
+		}
+		for !IsEnd(p) {
+			Advance(p)
+		}
 	}
 }
 
 func BenchmarkGenerator(b *testing.B) {
-	p, err := New(benchNum)
-	if err != nil {
-		b.Fatalf("New() failed: %v", err)
-	}
-	for !p.Done() {
-		p.Next()
+	for i := 0; i < b.N; i++ {
+		p, err := New(benchNum)
+		if err != nil {
+			b.Fatalf("New() failed: %v", err)
+		}
+		for !p.Done() {
+			p.Next()
+		}
 	}
 }
 
@@ -28,7 +32,9 @@ func BenchmarkPermRecursive(b *testing.B) {
 	n := benchNum
 	p := make([]int, n)
 	ignore := make([]bool, n)
-	recPerm(p, n, ignore)
+	for i := 0; i < b.N; i++ {
+		recPerm(p, n, ignore)
+	}
 }
 
 func recPerm(p []int, n int, ignore []bool) {
